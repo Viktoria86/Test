eb42c47fa544f8391a2fea89249a3cc93415f5ef
Viktoria86 <vika.zh86@bk.ru>
2021-12-09 17:43:57 +0300

    Модальное окно

---
+++ figma/script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.btn');
+const close_btn = document.querySelector('.close');
+const modal = document.querySelector('.modal');
+
+btn.addEventListener( 'click',  function (event)
+{
+    modal.classList.add('showed');
+
+}
+);
+
+close_btn.addEventListener( 'click',  function (event)
+{
+    modal.classList.remove('showed');
+
+}
+);
---
+++ media/js/1.js
@@ -0,0 +1,4 @@
+document.querySelector('#add').onclick=function() {
+//document.querySelector('#add').style.backgroundColor='#ff0000';
+
+}
